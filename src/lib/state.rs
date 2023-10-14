cdb6b8042bb21cc4760d166df736fb3622d690a1
Core <daverencordero@gmail.com>
2023-10-15 03:48:12 +0800

    feat: created a render pipeline

--- src/lib/state.rs
+++ src/lib/state.rs
@@ -4,24 +4,19 @@ pub struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     pub size: winit::dpi::PhysicalSize<u32>,
-    window: winit::window::Window
+    window: winit::window::Window,
+    pub render_pipeline: wgpu::RenderPipeline
 }
 
 impl State {
     pub async fn new(window: winit::window::Window) -> Self {
         let size = window.inner_size();
 
-        // The instance is a handle to our GPU
-        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
             dx12_shader_compiler: Default::default(),
         });
 
-        // # Safety
-        //
-        // The surface needs to live as long as the window that created it.
-        // State owns the window z this should be safe.
         let surface = unsafe { instance.create_surface(&window) }.unwrap();
 
         let adapter = instance.request_adapter(
@@ -35,8 +30,6 @@ impl State {
         let (device, queue) = adapter.request_device(
             &wgpu::DeviceDescriptor {
                 features: wgpu::Features::empty(),
-                // WebGL doesn't support all of wgpu's features, so if
-                // we're building for the web we'll have to disable some.
                 limits: if cfg!(target_arch = "wasm32") {
                     wgpu::Limits::downlevel_webgl2_defaults()
                 } else {
@@ -48,9 +41,7 @@ impl State {
         ).await.unwrap();
         
         let surface_caps = surface.get_capabilities(&adapter);
-        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
-        // one will result all the colors coming out darker. If you want to support non
-        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+
         let surface_format = surface_caps.formats.iter()
             .copied()
             .find(|f| f.is_srgb())            
@@ -65,7 +56,53 @@ impl State {
             view_formats: vec![],
         };
         surface.configure(&device, &config);
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor { 
+            label: Some("Shader"), 
+            source: wgpu::ShaderSource::Wgsl(include_str!("shaders/shader.wgsl").into())
+        });
         
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor { 
+            label: Some("Render Pipeline Layout"), 
+            bind_group_layouts: &[], 
+            push_constant_ranges: &[] 
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor { 
+            label: Some("Render Pipeline"), 
+            layout: Some(&render_pipeline_layout), 
+            vertex: wgpu::VertexState { 
+                module: &shader, 
+                entry_point: "vs_main", 
+                buffers: &[] 
+            },  
+            fragment: Some(wgpu::FragmentState { 
+                module: &shader, 
+                entry_point: "fs_main", 
+                targets: &[Some(wgpu::ColorTargetState { 
+                    format: config.format, 
+                    blend: Some(wgpu::BlendState::REPLACE), 
+                    write_mask: wgpu::ColorWrites::ALL 
+                })] 
+            }),
+            primitive: wgpu::PrimitiveState { 
+                topology: wgpu::PrimitiveTopology::TriangleList, 
+                strip_index_format: None, 
+                front_face: wgpu::FrontFace::Ccw, 
+                cull_mode: Some(wgpu::Face::Back), 
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill, 
+                conservative: false 
+            }, 
+            depth_stencil: None, 
+            multisample: wgpu::MultisampleState { 
+                count: 1, 
+                mask: !0, 
+                alpha_to_coverage_enabled: false 
+            }, 
+            multiview: None
+        });
+
         Self {
             window,
             surface,
@@ -73,6 +110,7 @@ impl State {
             queue,
             config,
             size,
+            render_pipeline
         }
     }
     
@@ -103,7 +141,7 @@ impl State {
             label: Some("Render Encoder"),
         });
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -120,11 +158,14 @@ impl State {
                 })],
                 depth_stencil_attachment: None,
             });
+
+            render_pass.set_pipeline(&self.render_pipeline); 
+            render_pass.draw(0..3, 0..1); 
         }
 
-        // submit will accept anything that implements IntoIter
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
+    
 
         Ok(())
     }
